361916a7c4bb64e8e8eda9090a0a7e6b3adf9efe
MLuisa Santos <mls_mansell@outlook.com>
2021-02-15 15:31:23 +0100

    done

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Header from './components/Header'
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+import Resumen from './components/Resumen';
+
+const Contenedor = styled.div`
+max-width: 600px;
+margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+background-color: #FFF;
+padding: 3rem;
+`;
+
+function App() {
+
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan:''
+    }
+  });
+
+  const { datos } = resumen;
+
+  return (
+    <Contenedor>
+      <Header
+        titulo='Cotizador de Seguros'
+      />
+      <ContenedorFormulario>
+        <Formulario guardarResumen={guardarResumen} />
+        <Resumen
+          datos={datos}
+        />
+      </ContenedorFormulario>
+    </Contenedor>
+
+  );
+}
+
+export default App;
---
+++ src/components/Resumen.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react'
+import styled from '@emotion/styled'
+
+const ContenedorResumen = styled.div`
+padding: 1rem;
+text-align:center;
+background-color:#00838F;
+color:FFF;
+margin-top: 1rem;
+`;
+
+const Resumen = (datos) => {
+    const { marca, year, plan } = datos;
+    if (marca === '' || year === '' || plan === '') return
+    null;
+    return (
+        <ContenedorResumen>
+            <h2>Resumen de cotización</h2>
+            <ul>
+                <li>Marca: {marca}</li>
+                <li>Plan: {plan}</li>
+                <li>Año del Auto:{year}</li>
+            </ul>
+        </ContenedorResumen>
+    );
+}
+
+
+export default Resumen;
